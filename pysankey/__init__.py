af95f9e560c9d385078adef1561bec15e1887423
Sulaiman <suleiman_hayek@yahoo.com>
2024-05-29 07:26:04 +0300

    add pysankey lib

---
+++ pysankey/__init__.py
@@ -0,0 +1,5 @@
+""" Make simple, pretty Sankey Diagrams """
+
+from pysankey.sankey import sankey, PySankeyException, NullsInFrame, LabelMismatch
+
+__all__ = ["sankey", "PySankeyException", "NullsInFrame", "LabelMismatch"]
